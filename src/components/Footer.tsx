be744c82b33e8fede5b925dda324ca0994f85539
Aleksey Verin <verevaa@yandex.ru>
2023-04-16 11:39:10 +0300

    feat: added to send a message by pressing Enter

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ImgClose from './image/ImgClose';
 
 interface FooterProps {
@@ -10,17 +10,28 @@ const defaultValue = '';
 const Footer = ({ sendMessage }: FooterProps) => {
   const [inputValue, setInputValue] = useState(defaultValue);
 
-  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleSubmit = () => {
     if (!inputValue.trim().length) return;
-    console.log(inputValue);
     sendMessage(inputValue);
     setInputValue(defaultValue);
   };
 
+  useEffect(() => {
+    const keyDownHandler = (event: KeyboardEvent) => {
+      if (event.key === 'Enter' && !event.shiftKey) {
+        event.preventDefault();
+        handleSubmit();
+      }
+    };
+    document.addEventListener('keydown', keyDownHandler);
+    return () => {
+      document.removeEventListener('keydown', keyDownHandler);
+    };
+  }, [inputValue]);
+
   return (
     <footer>
-      <form onSubmit={handleSubmit} className="send-message" id="sendMessage" action="">
+      <form className="send-message" id="sendMessage">
         <textarea
           className="textarea-message"
           placeholder="message.."
@@ -29,7 +40,7 @@ const Footer = ({ sendMessage }: FooterProps) => {
           value={inputValue}
           onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setInputValue(e.target.value)}
           form="sendMessage"></textarea>
-        <button className="btn-send" type="submit">
+        <button onClick={handleSubmit} className="btn-send" type="submit">
           <ImgClose />
         </button>
       </form>
@@ -38,3 +49,50 @@ const Footer = ({ sendMessage }: FooterProps) => {
 };
 
 export default Footer;
+
+// The classic form does not work for sending a message by pressing Enter
+//
+// const Footer = ({ sendMessage }: FooterProps) => {
+//   const [inputValue, setInputValue] = useState(defaultValue);
+//   const formRef = useRef<HTMLFormElement>(null);
+
+//   const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+//     if (!inputValue.trim().length) return;
+//     sendMessage(inputValue);
+//     setInputValue(defaultValue);
+//   };
+
+//   const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+//     if (e.key === 'Enter') {
+//       e.preventDefault();
+//       formRef.current?.dispatchEvent(new Event('submit'));
+//     }
+//   };
+
+//   return (
+//     <footer>
+//       <form
+//         onSubmit={handleSubmit}
+//         className="send-message"
+//         id="sendMessage"
+//         action=""
+//         ref={formRef}>
+//         <textarea
+//           className="textarea-message"
+//           placeholder="message.."
+//           autoComplete="off"
+//           autoFocus
+//           value={inputValue}
+//           onKeyDown={handleKeyDown}
+//           onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setInputValue(e.target.value)}
+//           form="sendMessage"></textarea>
+//         <button className="btn-send" type="submit">
+//           <ImgClose />
+//         </button>
+//       </form>
+//     </footer>
+//   );
+// };
+
+// export default Footer;
